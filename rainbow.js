447ec891f7efdbb268fd3746f73f7622f7966a18
Matthew Molloy <whamtet@gmail.com>
2022-11-26 08:04:38 +0900

    fixed color bug

--- rainbow.js
+++ rainbow.js
@@ -1,6 +1,5 @@
-const piecewise_ = i => i < 512 ? Math.min(i, 255) : Math.max(0, 768 - i);
-const mod = (a, b) => a > 0 ? a % b : (a % b) + b;
-const piecewise = x => piecewise_(mod(x, 1024));
+const piecewise_ = i => i < 768 ? Math.min(i, 255) : Math.max(0, 1024 - i);
+const piecewise = x => piecewise_(x % 1536);
 
 let c = 0;
 const div = document.getElementById('rainbow');
@@ -8,8 +7,8 @@ const div = document.getElementById('rainbow');
 setInterval(() => {
     c++;
     const r = piecewise(c);
-    const g = piecewise(c - 256);
-    const b = piecewise(c - 512);
+    const g = piecewise(c + 512);
+    const b = piecewise(c + 1024);
 
     div.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 }, 50);
